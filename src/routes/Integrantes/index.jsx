a721d1486c388cef15c108ee00306a92635cfe54
Gabriell Hernandes Dassi <126270625+gabriellhernandesdassi@users.noreply.github.com>
2023-09-12 21:28:55 -0300

    Começando index integrantes

---
+++ src/routes/Integrantes/index.jsx
@@ -0,0 +1,13 @@
+index na pasta Integrantes
+
+import nicolas from "../../img/omaisgato.jpg";
+import luis from "../../img/luis.jpg";
+import thiago from "../../img/thigaz.jpg";
+import pedro from "../../img/pedro.jpg";
+import denner from "../../img/denden.jpg";
+import pamelo from "../../img/diciotech.jpg";
+
+export default function Integrantes() {
+  return (
+    <div className="integrantes-main">
+      <h1 className="subtitulo">Integrantes</h1>
