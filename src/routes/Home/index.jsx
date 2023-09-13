1348238a301233cde1120159a630370da40cd8a7
ThiagoAgostinho <thiagor793@gmail.com>
2023-09-12 21:28:45 -0300

    Qual objetivo do projeto

--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -11,6 +11,17 @@ export default function Janos() {
               segurança a seguradora ao concluir a contratação do seguro.
             </p>
           </div>
+          <div>
+            <h2 className="titulo-janos">Qual o objetivo do projeto?</h2>
+            <p className="texto-janos">
+              Utilizar uma Inteligência Artificial para contactar o usuário, que
+              irá validar dados sobre o usuário, e será fornecido uma aplicação
+              para ser utilizada a câmera do celular para o envio de fotos da
+              bicicleta, esses dados serão validados pelo banco de dados e
+              certificará que a bicicleta realmente condiz com a bicicleta real.
+            </p>
+          </div>
+  
           
         </div>
       </>
