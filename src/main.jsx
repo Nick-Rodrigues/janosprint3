d53bc3a7f3f5e7344468cf430202d6db8d2ffc57
Nick-Rodrigues <RM551060@fiap.com.br>
2023-09-12 21:12:21 -0300

    Rotas com BrowserRouter.

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,28 @@ import Etapas from "./routes/Etapas/index.jsx"
 import Home from "./routes/Home/index.jsx"
 import Integrantes from './routes/Integrantes/index.jsx'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    errorElement: <Error/>,
+    children :[
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/etapas",
+        element: <Etapas/>
+      },
+      {
+        path: "/integrantes",
+        element: <Integrantes/>
+      }
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
