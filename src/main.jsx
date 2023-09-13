1279175fcfd22987a8a7cb158e5f85dd63730bfe
Nick-Rodrigues <RM551060@fiap.com.br>
2023-09-12 21:11:33 -0300

    Imports de rotas realizados.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import App from './App.jsx'
-import './index.css'
+import Error from "./routes/Error/index.jsx"
+import Etapas from "./routes/Etapas/index.jsx"
+import Home from "./routes/Home/index.jsx"
+import Integrantes from './routes/Integrantes/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
