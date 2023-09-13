b61fc84670bf036ec368817d1490e86997d81f7f
Gabriell Hernandes Dassi <126270625+gabriellhernandesdassi@users.noreply.github.com>
2023-09-12 21:29:49 -0300

    index.jsx versão final

--- src/routes/Integrantes/index.jsx
+++ src/routes/Integrantes/index.jsx
@@ -11,3 +11,32 @@ export default function Integrantes() {
   return (
     <div className="integrantes-main">
       <h1 className="subtitulo">Integrantes</h1>
+      <div>
+        <figure>
+          <img src={nicolas} className="nicolas" alt="nicolas" />
+          <figcaption>rm-551060</figcaption>
+        </figure>
+        <figure>
+          <img src={pedro} className="pedro" alt="pedro" />
+          <figcaption>rm-551409</figcaption>
+        </figure>
+        <figure>
+          <img src={luis} className="luis" alt="luís" />
+          <figcaption>rm-99433</figcaption>
+        </figure>
+        <figure>
+          <img src={denner} className="denner" alt="denden" />
+          <figcaption>rm-551938</figcaption>
+        </figure>
+        <figure>
+          <img src={thiago} className="thiago" alt="thiago" />
+          <figcaption>rm-99027</figcaption>
+        </figure>
+        <figure>
+          <img src={pamelo} className="gabriel" alt="diciotech" />
+          <figcaption>rm-98361</figcaption>
+        </figure>
+      </div>
+    </div>
+  )
+}
