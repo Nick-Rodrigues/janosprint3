221ba2412b6b938ae9c4caff3216d90edd17aece
ThiagoAgostinho <thiagor793@gmail.com>
2023-09-12 21:28:17 -0300

    O que é Jano's Enterprise

---
+++ src/routes/Home/index.jsx
@@ -0,0 +1,19 @@
+export default function Janos() {
+    return (
+      <>
+        <div className="main-div">
+          <div>
+            <h2 className="titulo-janos">O que é a Jano’s Enterprise?</h2>
+            <p className="texto-janos">
+              A Jano’s surgiu como uma empresa parceira da Porto Seguro com
+              objetivo de automatizar o processo de vistoria de bicicletas,
+              fazendo com que gere uma facilidade ao cliente juntamente com uma
+              segurança a seguradora ao concluir a contratação do seguro.
+            </p>
+          </div>
+          
+        </div>
+      </>
+    )
+  }
+  
